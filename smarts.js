4ad6b0b54e0b427ff0126f520038fb427b27288f
Tech IT Smart <121350744+tech-it-smart@users.noreply.github.com>
2023-05-24 22:27:52 +0530

    line 7 modified

--- smarts.js
+++ smarts.js
@@ -4,6 +4,10 @@ let currentDate = new Date().toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
     day: "numeric"
+}) + " - " + new Date().toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
 })
 localStorage.setItem(pageTitle, currentDate)
 
