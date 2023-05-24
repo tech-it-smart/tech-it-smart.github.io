cbc1b0b1ecbed081930c096cf0efe227d34e6432
Suryansh-Parashar-26 <sparashar2002@gmail.com>
2023-05-24 21:39:40 +0530

    Deploying through master branch to test local storage on phone

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
-const smartLogo = document.querySelector("#smart-logo")
-smartLogo.src = "https://tech-it-smart.github.io/assets/logo.png"
+// Storing Accessing Dates
+const pageTitle = document.title
+const currentDate = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+}) + " - " + new Date().toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+})
+localStorage.setItem(pageTitle, currentDate)
 
-alert("Under Development")
+// Notification
+if (Notification.permission === "granted") {
+    showNotification()
+} else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+            showNotification()
+        }
+    })
+}
+function showNotification() {
+    if ("Notification" in window || "serviseWorker" in navigator) {
+        const notify = new Notification("Alert", {
+            body: "Notification Body",
+            icon: "/assets/logo-small.png"
+        })
+        notify.onclick = function() {
+            location.href = "https://discord.gg/HEbRJVaqp2"
+            console.log("Notification Clicked")
+            localStorage.setItem("Notification clicked", currentDate)
+        }
+    }
+    console.log(Notification.permission)
+}
+const accessLogs = localStorage.getItem(pageTitle, currentDate)
+console.log(accessLogs)
 
-function cardContent() {
-    const texts = document.querySelectorAll(".card-text")
-    for (const text of texts) {
-        console.log(text);
-        text.innerHTML = "Under Development"
-    }
-    
-    const dates = document.querySelectorAll(".card-date")
-    for (const d of dates) {
-        d.innerText = "07-05-2023"
-    }
+
+
+
+const allSolutionsCard = document.querySelector("#all-solutions")
+allSolutionsCard.addEventListener("click", allSolutions)
+function allSolutions() {
+    location.href="/solutions/"
 }
 
-const cards = document.querySelectorAll(".cards")
-for (const card of cards) {
-    console.log(card);
-    card.addEventListener("click", cardContent)
+
+
+const allToolsCard = document.querySelector("#all-tools")
+allToolsCard.addEventListener("click", allTools)
+function allTools() {
+    location.href="/tools/"
 }
+
+
+/*
+const allWebDevCard = document.querySelector("#all-web-dev-cards")
+allWebDevCard.addEventListener("click", allWebDevTopics)
+function allWebDevTopics() {
+    location.href="/web-dev/"
+}
+*/
+
+
